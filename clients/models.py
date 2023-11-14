e07f3723edbea8b85dc183c84185dc04b689a44c
Omar-Alme <90763798+Omar-Alme@users.noreply.github.com>
2023-11-14 02:07:15 +0100

    Initial commit

--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+# class client(models.Model):
+#     profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
+#     first_name = models.CharField(max_length=50)
+#     last_name = models.CharField(max_length=50)
+#     business_name = models.CharField(max_length=50)
+#     email = models.CharField(max_length=50)
+#     bio = models.TextField(max_length=500)
+#     tasks = models.TextField(max_length=500)
+#     task_status = models.TextField(max_length=500)
+
+#     def __str__(self):
+#         return f"{self.first_name} {self.last_name}"
--- owners/models.py
+++ owners/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+# class Owner(models.Model):
+#     profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
+#     first_name = models.CharField(max_length=50)
+#     last_name = models.CharField(max_length=50)
+#     business_name = models.CharField(max_length=50)
+#     email = models.CharField(max_length=50)
+#     bio = models.TextField(max_length=500)
+
+#     def __str__(self):
+#         return f"{self.first_name} {self.last_name}"
