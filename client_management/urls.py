eeef9e5788662684adae352c51c7b6fe46500fcf
Omar-Alme <90763798+Omar-Alme@users.noreply.github.com>
2023-11-16 22:43:22 +0100

    Add urls of all apps

--- client_management/urls.py
+++ client_management/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-# from django.conf.urls import include
+from django.urls import path, include
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-#     path('', include("owners.urls")),
-#     path('', include("clients.urls")),
+    path('', include("home.urls")),
+    path('owners/', include("owners.urls")),
+    path('clients/', include("clients.urls")),
 ]
