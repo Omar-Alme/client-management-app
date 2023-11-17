d891bcc2558cc72af96939194d235f357a945429
Omar-Alme <90763798+Omar-Alme@users.noreply.github.com>
2023-11-17 01:24:17 +0100

    Add filters to admin for clients

--- clients/admin.py
+++ clients/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import client
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(client)
+
+@admin.register(client)
+class clientAdmin(SummernoteModelAdmin):
+    summernote_fields = ('notes',)
+    list_display = ('Client_name', 'email', 'industry', 'Online_Presence')
+    list_filter = ('industry', 'online_presence')
+    search_fields = ('client_name', 'industry', 'online_presence')
