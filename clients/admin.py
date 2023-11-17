052bc48566128b4a3a01a298302a9dfd5aac4e05
Omar-Alme <90763798+Omar-Alme@users.noreply.github.com>
2023-11-17 01:30:54 +0100

    Fix typos

--- clients/admin.py
+++ clients/admin.py
@@ -6,6 +6,6 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(client)
 class clientAdmin(SummernoteModelAdmin):
     summernote_fields = ('notes',)
-    list_display = ('Client_name', 'email', 'industry', 'Online_Presence')
+    list_display = ('client_name', 'email', 'industry', 'online_presence')
     list_filter = ('industry', 'online_presence')
     search_fields = ('client_name', 'industry', 'online_presence')
