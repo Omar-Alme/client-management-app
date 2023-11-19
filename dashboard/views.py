41562ce9797424b915c009cf7633d9f5f957f94d
Omar-Alme <90763798+Omar-Alme@users.noreply.github.com>
2023-11-19 23:19:09 +0100

    Add dashboard and client add button, add views to required login

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required 
 
 
-"""A view that displays the dashboard page"""
+"""A view that displays the dashboard page only when logged in"""
+@login_required
 def dashboard(request):
-    """A view that displays the index page"""
     return render(request, 'dashboard/dashboard.html')
