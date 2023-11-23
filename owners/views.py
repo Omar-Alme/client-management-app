9f69d12fc0b0362abc99ea1f3d90a57a36485c36
Omar-Alme <90763798+Omar-Alme@users.noreply.github.com>
2023-11-23 11:46:25 +0100

    Render owner/user to profile page

--- owners/views.py
+++ owners/views.py
@@ -1,21 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from owners.models import owner
+from owners.models import Owner
 
 
 # Create your views here.
 def profile(request):
     """A view that displays the profile page"""
-    return render(request, 'owners/profile.html')
+    owner = get_object_or_404(Owner, user=request.user)
+    context = {
+        'owner': owner,
+    }
+    return render(request, 'owners/profile.html', context)
 
-def edit_profile(request):
-    """A view that displays the edit profile page"""
-    return render(request, 'owners/edit_profile.html')
 
 class ownerProfileView(View):
     """A view that displays the owner profile page"""
     def get(self, request):
-        profile = owner.objects.all()
         return render(request, 'owners/profile.html')
