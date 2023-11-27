9094b49c8074a42b17d412097e41877bb9b197aa
Omar-Alme <90763798+Omar-Alme@users.noreply.github.com>
2023-11-27 02:00:24 +0100

    Connect Signal handlers

--- owners/apps.py
+++ owners/apps.py
@@ -4,3 +4,11 @@ from django.apps import AppConfig
 class OwnersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'owners'
+
+
+def ready(self):
+        from owners.signals import create_profile
+        from django.contrib.auth.models import User
+        post_save.connect(create_profile, sender=User)
+        from owners.signals import save_profile
+        post_save.connect(save_profile, sender=User)
