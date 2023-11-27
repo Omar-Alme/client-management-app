f9ab1e5f68886b69845ad6bcf7d5c7a464712bc5
Omar-Alme <90763798+Omar-Alme@users.noreply.github.com>
2023-11-27 21:59:33 +0100

    Add urls and views to client form when add client pressed

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
   path('', views.dashboard, name='dashboard'),
+  path('client_form/', views.client_form, name='client_form'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from owners.models import owner
+from owners.models import Owner
 from django.contrib.auth.decorators import login_required 
+from django.http import HttpResponse, JsonResponse
+from clients.models import Client
+from .forms import ClientForm
 
 
 """A view that displays the dashboard page only when logged in"""
@@ -9,3 +12,15 @@ def dashboard(request):
     """Retrieve the user's profile information"""
 
     return render(request, 'dashboard/dashboard.html')
+
+def client_form(request):
+    """A view that displays the client form"""
+   
+
+    return render(request, 'clients/client_form.html')
+
+
+           
+
+    
+    
