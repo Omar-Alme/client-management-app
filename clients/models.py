818a6b3a96d4117cba627ca295c2ee5140569817
Omar-Alme <90763798+Omar-Alme@users.noreply.github.com>
2023-12-07 22:09:48 +0100

    Reduce max characters in bio

--- clients/models.py
+++ clients/models.py
@@ -7,7 +7,7 @@ class Client(models.Model):
     client_name = models.CharField(max_length=50, blank=True, null=True)
     industry = models.CharField(max_length=50, blank=True, null=True)
     email = models.EmailField(max_length=50, unique=True, blank=True, null=True)
-    bio = models.TextField(max_length=500, blank=True, null=True, default="No bio yet")
+    bio = models.TextField(max_length=250, blank=True, null=True, default="No bio yet")
     online_presence = models.URLField(max_length=200, blank=True, null=True)
 
    
